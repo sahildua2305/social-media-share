5312577de16f9d175d7d746856629b17fc2dc338
Sahil <sahildua2305@gmail.com>
2014-07-15 04:17:43 +0530

    fb-like implemented - tested

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,9 @@ function shareCurrentURL(){
 		document.getElementById('url').innerHTML = url;
 		var tweetCode = '<a target="_blank" href="https://twitter.com/intent/tweet?text='+encodeURIComponent(title)+'&url='+encodeURIComponent(url)+'">Tweet</a>';
 		document.getElementById('twitter').innerHTML = tweetCode;
+		
+		var fbLikeCode = '<iframe src="http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&send=false&layout=button_count&width=120&show_faces=true&font&colorscheme=light&action=like&height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:20px;" allowTransparency="true"></iframe>';
+		document.getElementById('fb-like').innerHTML = fbLikeCode;
 	});
 }
 
