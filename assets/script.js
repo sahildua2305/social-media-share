edad88d6bf2c20eb158eb654ff610024f61d1fb1
Sahil <sahildua2305@gmail.com>
2014-07-15 04:02:52 +0530

    twitter share implemented

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 
-function getCurrentURL(){
+function shareCurrentURL(){
 	chrome.tabs.query({
 		active: true,
 		lastFocusedWindow: true
 	}, function(array_of_tabs){
 		var tab = array_of_tabs[0];
 		var url = tab.url;
-		document.write(url);
+		var title = tab.title;
+		document.getElementById('url').innerHTML = url;
+		var tweetCode = '<a target="_blank" href="https://twitter.com/intent/tweet?text='+encodeURIComponent(title)+'&url='+encodeURIComponent(url)+'">Tweet</a>';
+		document.getElementById('twitter').innerHTML = tweetCode;
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-	getCurrentURL();
+	shareCurrentURL();
 });
