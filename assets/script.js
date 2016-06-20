0d3123fc0e77b5296ab7fc499d9c5eeddd307fa3
Sahil Dua <sahildua2305@gmail.com>
2016-06-21 00:42:30 +0530

    Update to version 1.0.2

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ function shareCurrentURL() {
         var tab = array_of_tabs[0];
         var url = tab.url;
         var title = tab.title;
-        //document.getElementById('url').innerHTML = url;
 
         var tweetCode = '<a target="_blank" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodeURIComponent(url) + '"><img src="' + chrome.extension.getURL('assets/share-icons/twitter.png') + '"></a>';
         document.getElementById('twitter').innerHTML = tweetCode;
