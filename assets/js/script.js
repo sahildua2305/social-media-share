35027cf73879544947a215671dd09f13637ef4d4
Sahil Dua <sahildua2305@gmail.com>
2016-06-21 00:33:23 +0530

    Replace with high quality icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,13 @@ function shareCurrentURL() {
         var tweetCode = '<a target="_blank" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodeURIComponent(url) + '"><img src="' + chrome.extension.getURL('assets/share-icons/twitter.png') + '"></a>';
         document.getElementById('twitter').innerHTML = tweetCode;
 
+        // var fbLikeCode = '<iframe src="http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&send=false&layout=button_count&width=120&show_faces=true&font&colorscheme=light&action=like&height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:20px;" allowTransparency="true"></iframe>';
+        // document.getElementById('fb-like').innerHTML = fbLikeCode;
+
         var fbShareCode = '<a href="https://www.facebook.com/sharer/sharer.php?u=' + url + '" target="_blank"><img src="' + chrome.extension.getURL('assets/share-icons/facebook.png') + '"></a>';
         document.getElementById('fb-share').innerHTML = fbShareCode;
 
-        var gplusShareCode = '<a href="https://plus.google.com/share?url=' + url + '" target="_blank"><img src="' + chrome.extension.getURL('assets/share-icons/gplus.png') + '"></a>';
+        var gplusShareCode = '<a href="https://plus.google.com/share?url=' + url + '" target="_blank"><img src="' + chrome.extension.getURL('assets/share-icons/gplus2.png') + '"></a>';
         document.getElementById('gp-share').innerHTML = gplusShareCode;
 
         var linkedInShareCode = '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + url + '&title=' + title + '&summary=&source=" target="_blank"><img src="' + chrome.extension.getURL('assets/share-icons/linkedin.png') + '"></a>';
